eedf6fd1bf23398675e1ee0a3999cd78f463f033
Maurício de Lima <mauriciodelima.mol@gmail.com>
2023-02-02 19:46:24 -0300

    Update 00-prepara.sh

--- 00-prepara.sh
+++ 00-prepara.sh
@@ -12,3 +12,26 @@ for dir in "${dirs[@]}"; do
   fi
 done
 
+# adiciona a linha para chamar os arquivos em /etc/network/interfaces
+gre_sh_line="up /opt/uai/gre.sh"
+firewall_sh_line="up /opt/uai/firewall.sh"
+router_sh_line="up /opt/uai/router.sh"
+
+if grep -q "$gre_sh_line" /etc/network/interfaces; then
+  echo "Line already exists in /etc/network/interfaces"
+else
+  echo "$gre_sh_line" >> /etc/network/interfaces
+fi
+
+if grep -q "$firewall_sh_line" /etc/network/interfaces; then
+  echo "Line already exists in /etc/network/interfaces"
+else
+  echo "$firewall_sh_line" >> /etc/network/interfaces
+fi
+
+if grep -q "$router_sh_line" /etc/network/interfaces; then
+  echo "Line already exists in /etc/network/interfaces"
+else
+  echo "$router_sh_line" >> /etc/network/interfaces
+fi
+
