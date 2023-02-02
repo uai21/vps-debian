5037e22a257f2853c0c59c75cbc6e317b07f0caf
Maurício de Lima <mauriciodelima.mol@gmail.com>
2023-02-02 18:46:01 -0300

    Update 01-instala-gre.sh

--- 01-instala-gre.sh
+++ 01-instala-gre.sh
@@ -5,8 +5,8 @@ if dpkg -s iproute2 &>/dev/null; then
   echo "Pacote iproute2 já está instalado."
 else
   echo "Instalando pacote iproute2..."
-  sudo apt-get update
-  sudo apt-get install iproute2
+  apt-get update
+  apt-get install iproute2
 fi
 
 # Verifica se o módulo GRE já está carregado
@@ -14,7 +14,7 @@ if lsmod | grep -q ip_gre; then
   echo "Módulo ip_gre já está carregado."
 else
   echo "Carregando módulo ip_gre..."
-  sudo modprobe ip_gre
+  modprobe ip_gre
 fi
 
 # Verifica se a linha ip_gre já está no arquivo de configuração do kernel
@@ -22,7 +22,7 @@ if grep -q "ip_gre" /etc/modules; then
   echo "Linha ip_gre já está no arquivo de configuração do kernel."
 else
   echo "Adicionando linha ip_gre ao arquivo de configuração do kernel..."
-  echo "ip_gre" | sudo tee -a /etc/modules >/dev/null
+  echo "ip_gre" | tee -a /etc/modules >/dev/null
 fi
 
 echo "Módulo ip_gre está pronto para ser usado."
